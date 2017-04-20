97ee204fbeb37b1f4f17b725eca807c2ce4560d8
souzen13 <baptiste.franel@outlook.fr>
2017-04-20 09:30:58 +0200

    Create sv_admin.lua

---
+++ sv_admin.lua
@@ -0,0 +1,33 @@
+-- Add into sv_admin in [essential] folder
+
+
+local permission = {
+	kick = 1,
+	normal = 0,
+	ban = 3
+}
+
+
+-- 911 DISPATCHER
+TriggerEvent('es:addAdminCommand', 'dispatch', permission.ban, function(source, args, user)
+	table.remove(args, 1)
+	TriggerClientEvent('chatMessage', -1, "^5[911]", {30, 144, 255}, " (^1 Dispatcher: ^3" .. GetPlayerName(source) .." | "..source.."^0 ) " .. table.concat(args, " "))
+end, function(source, args, user)
+	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficienct permissions!")
+end)
+
+-- 911 CALL
+TriggerEvent('es:addAdminCommand', '911', permission.normal, function(source, args, user)
+	table.remove(args, 1)
+	TriggerClientEvent('chatMessage', -1, "^5[911]", {30, 144, 255}, " (^1 Caller ID: ^3" .. GetPlayerName(source) .." | "..source.."^0 ) " .. table.concat(args, " "))
+end, function(source, args, user)
+	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficienct permissions!")
+end)
+
+-- ME
+TriggerEvent('es:addAdminCommand', 'me', permission.normal, function(source, args, user)
+	table.remove(args, 1)
+	TriggerClientEvent('chatMessage', -1, "^3[Me]", {255, 0, 0}, " (^2 " .. GetPlayerName(source) .." | "..source.."^0 ) " .. table.concat(args, " "))
+end, function(source, args, user)
+	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficienct permissions!")
+end)
